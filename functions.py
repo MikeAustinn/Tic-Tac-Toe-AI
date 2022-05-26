ee086cf7582f10fa9ce5dafc624c245562fa559d
MikeAustinn <103225984+MikeAustinn@users.noreply.github.com>
2022-05-26 15:45:56 -0700

    Update functions.py

--- functions.py
+++ functions.py
@@ -23,6 +23,27 @@ def update():
     Return: Updated Board(s) """
     pass
 
-def win_check():
-    # Only needs to be used 5 moves in
-    pass
+def win_check(board):
+    sign = 1
+    while sign <= 2:
+        return((board[0]== sign and board[1]==  sign and board[2]== sign )or #for row1 
+
+                (board[3]== sign and board[4]== sign and board[5]== sign )or #for row2
+
+                (board[6]== sign and board[7]== sign and board[8]== sign )or #for row3
+
+                (board[0]== sign and board[3]== sign and board[6]==  sign )or#for Colm1 
+
+                (board[1]== sign and board[4]== sign and board[7]== sign )or #for Colm 2
+
+                (board[2]== sign and board[5]== sign and board[8]== sign )or #for colm 3
+
+                (board[0]== sign and board[4]== sign and board[8]== sign )or #daignole 1
+
+                (board[2]== sign and board[4]== sign and board[6]== sign )) #daignole 2
+
+    sign += 1
+
+    return 0
+
+win_check
