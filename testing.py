2bb34a136f8abbd6f312c0ad2ed5e709ef8364fc
MikeAustinn <103225984+MikeAustinn@users.noreply.github.com>
2022-05-26 15:46:52 -0700

    Update testing.py

--- testing.py
+++ testing.py
@@ -12,8 +12,16 @@ class FunctionTesting(unittest.TestCase):
         self.assertEqual(f.take_move(old, 1, 1), new)
 
     def test_win_check(self):
-        x_win = [1, 1, 1, 0, 2, 0, 0, 0, 2]
+        o_win = [2, 2, 2, 0, 1, 0, 0, 1, 1]
+        self.assertEqual(f.win_check(o_win), 2)
+        x_win = [1, 1, 1, 0, 2, 0, 0, 2, 0]
         self.assertEqual(f.win_check(x_win), 1)
+        no_win = [1, 2, 1, 1, 2, 2, 0, 2, 1]
+        self.assertEqual(f.win_check(no_win), 0)
 
 if __name__ == '__main__':
     unittest.main()
+
+    
+    
+    
